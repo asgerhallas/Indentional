4dd0a15c66116432288757530ffd0e31f37f619e
Asger Hallas <ahl@asgerhallas.dk>
2016-09-29 11:18:29 +0200

    init

---
+++ Indentional.Tests/IndentionalTests.cs
@@ -0,0 +1,84 @@
+﻿using Xunit;
+
+namespace Indentional.Tests
+{
+    public class IndentionalTests
+    {
+        [Fact]
+        public void RemovesCommonIndention()
+        {
+            var actual = Indentional._(@"
+                my first line
+                my second line");
+
+            Assert.Equal(@"my first line
+my second line", actual);
+        }
+
+        [Fact]
+        public void CalculatesCommonIndentFromFirstLineWithText()
+        {
+            var actual = Indentional._(@"
+
+                my first line
+                my second line");
+
+            Assert.Equal(@"
+my first line
+my second line", actual);
+        }
+
+        [Fact]
+        public void IfFirstLineHasNoIndentItCalculatesCommonIndentFromSecondLineWithText()
+        {
+            var actual = Indentional._(
+              @"my first line
+                my second line
+                my third line");
+
+            Assert.Equal(@"my first line
+my second line
+my third line", actual);
+        }
+
+        [Fact]
+        public void KeepsFurtherIdentation()
+        {
+            var actual = Indentional._(@"
+                my first line
+                    my second line
+                my third line");
+
+            Assert.Equal(@"my first line
+    my second line
+my third line", actual);
+        }
+
+        [Fact]
+        public void HandlesEmptyString()
+        {
+            var actual = Indentional._(@"");
+
+            Assert.Equal(@"", actual);
+        }
+
+        [Fact]
+        public void HandlesStringWithLineBreak()
+        {
+            var actual = Indentional._(@"
+");
+
+            Assert.Equal(@"", actual);
+        }
+
+        [Fact]
+        public void HandlesStringWithLineBreak2()
+        {
+            var actual = Indentional._(@"hallo
+");
+
+            Assert.Equal(@"hallo
+", actual);
+        }
+    }
+}
---
+++ Indentional/Indentional.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Indentional
+{
+    public static class Indentional
+    {
+        public static string _(string s)
+        {
+            if (s == null) return null;
+
+            var builder = new StringBuilder();
+            var reader = new StringReader(s);
+            var line = reader.ReadLine();
+
+            if (line == null) return s;
+
+            if (line.Trim().Length > 0)
+            {
+                builder.AppendLine(line);
+            }
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.Trim().Length == 0)
+                {
+                    builder.AppendLine();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (line == null)
+            {
+                return builder.ToString();
+            }
+
+            var trimmed = line.TrimStart();
+            var indent = line.Length - trimmed.Length;
+
+            builder.Append(trimmed);
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                builder.AppendLine();
+                builder.Append(line.Remove(0, indent));
+            } 
+
+            return builder.ToString();
+        }
+    }
+}
